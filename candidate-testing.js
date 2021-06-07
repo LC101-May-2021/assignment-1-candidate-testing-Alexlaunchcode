6a108c7cf9a7f3e611c07832d75a25dc9119d01a
Alex Schmitt <>
2021-06-07 07:46:25 +0000

    Changed it to try to make github happy

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,8 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " " ;
 let questions = ["Who was the first American women in space? ", "True or False: 5 kilometers == 5000 meters? ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["sally ride", "true", "40", "trajectory", "3"];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let correctAnswersLowerCase = ["sally ride", "true", "40", "trajectory", "3"];
 let candidateAnswers = []
 let candidateAnswersLowerCase = [];
 
@@ -50,7 +51,7 @@ function gradeQuiz(candidateAnswers) {
    // }
   let grade =0;
   for (let i = 0; i <questions.length; i++){
-    if (candidateAnswersLowerCase[i] === correctAnswers[i]){
+    if (candidateAnswersLowerCase[i] === correctAnswersLowerCase[i]){
     grade +=1
     }
     
